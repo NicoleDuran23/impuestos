38fd882c52eb8b4ac604541d7ed8b51c9c0f3289
unknown <nickyduran80@gmail.com>
2020-08-26 18:51:00 -0400

    impuestos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  dato = 0;
+  iva = 0;
+  it = 0;
+  utilidad=0;
+  resultado: string;
+
   constructor() {}
+
+  calcularImpuesto() {
+
+    this.iva = this.dato*(13/100) ;
+    this.resultado ='iva'; 
+   
+    this.it = this.dato*(3/100) ;
+    this.resultado ='it';
+ 
+    this.utilidad = this.dato - this.iva - this.it;
+    this.resultado ='utilidad';
+    
+   
+  }
+
+ 
 
 }
